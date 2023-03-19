d75ec0aedc54682df84f9fefbefb8f73e7537b54
Haidarradam <haidaradam123@gmail.com>
2023-03-20 02:02:46 +0700

    Fixing Same Bug

--- server/handlers/product.go
+++ server/handlers/product.go
@@ -162,6 +162,21 @@ func (h *handlerProduct) UpdateProduct(c echo.Context) error {
 			return c.JSON(http.StatusBadRequest, dto.ErrorResult{Status: http.StatusBadRequest, Message: err.Error()})
 		}
 
+		var ctx = context.Background()
+		var CLOUD_NAME = os.Getenv("CLOUD_NAME")
+		var API_KEY = os.Getenv("API_KEY")
+		var API_SECRET = os.Getenv("API_SECRET")
+
+		// Add your Cloudinary credentials ...
+		cld, _ := cloudinary.NewFromParams(CLOUD_NAME, API_KEY, API_SECRET)
+
+		// Upload file to Cloudinary ...
+		resp, err := cld.Upload.Upload(ctx, dataFile, uploader.UploadParams{Folder: "waysbeans"})
+
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+
 		if request.Name != "" {
 			product.Name = request.Name
 		}
@@ -172,20 +187,7 @@ func (h *handlerProduct) UpdateProduct(c echo.Context) error {
 			product.Price = request.Price
 		}
 		if request.Photo != "" {
-			fileName := product.Photo
-			dirPath := "uploads"
-
-			filePath := fmt.Sprintf("%s/%s", dirPath, fileName)
-
-			err = os.Remove(filePath)
-			if err != nil {
-				fmt.Println("Failed to delete file"+fileName+":", err)
-				return c.JSON(http.StatusInternalServerError, dto.ErrorResult{Status: http.StatusInternalServerError, Message: err.Error()})
-			}
-
-			fmt.Println("File " + fileName + " deleted successfully")
-
-			product.Photo = request.Photo
+			product.Photo = resp.SecureURL
 		}
 		if request.Stock != 0 {
 			product.Stock = request.Stock
