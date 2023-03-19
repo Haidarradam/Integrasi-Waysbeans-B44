e965b93f664f67378a8d5b38dc7202ba25650979
Haidarradam <haidaradam123@gmail.com>
2023-03-20 00:15:52 +0700

    Midtrans Done

--- server/handlers/transaction.go
+++ server/handlers/transaction.go
@@ -252,7 +252,7 @@ func SendMail(status string, transaction models.Transaction) {
 
 		mailer := gomail.NewMessage()
 		mailer.SetHeader("From", CONFIG_SENDER_NAME)
-		mailer.SetHeader("To", "bahanbelajardw@gmail.com")
+		mailer.SetHeader("To", transaction.Email)
 		mailer.SetHeader("Subject", "Transaction Status")
 		mailer.SetBody("text/html", fmt.Sprintf(`<!DOCTYPE html>
     <html lang="en">
